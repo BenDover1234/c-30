de6c839eb8d3aff905775097f091b1aa6b917d14
BenDover1234 <81786642+BenDover1234@users.noreply.github.com>
2021-08-23 19:25:51 -0600

    Update BoxBlue.js

--- BoxBlue.js
+++ BoxBlue.js
@@ -25,7 +25,7 @@ class Box {
              image(this.image, pos.x, pos.y, this.width, this.height);
         }
         else{
-            World.remove(smallWorld, this.body)
+            World.remove(world, this.body)
             push();
             this.visibility = this.visibility - 5;
             tint(255, this.visibility);
